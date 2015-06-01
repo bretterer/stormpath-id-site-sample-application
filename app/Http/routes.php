e0d655991c452992e7fc3e3605aa36e9001313fc
Brian Retterer <bretterer@gmail.com>
2015-06-01 09:32:54 -0400

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Config;
+$spid = $_ENV['STORMPATH_ID'];
+$spsecret = $_ENV['STORMPATH_SECRET'];
+\Stormpath\Client::$apiKeyProperties = "apiKey.id=$spid\napiKey.secret=$spsecret";
+$spapplication = \Stormpath\Resource\Application::get($_ENV['STORMPATH_APPLICATION']);
+
+
+
+$app->get('/', function() use ($app) {
+    $app->configure('app');
+    dd(env('STORMPATH_ID'));
+    dd(Config::get('app.locale'));
+    return view('welcome');
+});
+
+$app->get('login', function() use ($spapplication) {
+    $url = $spapplication->createIdSiteUrl(['callbackUri'=>'http://localhost:8000/idSiteResponse']);
+    return redirect($url);
+});
+
+$app->get('register', function() use ($spapplication) {
+    $url = $spapplication->createIdSiteUrl(['path'=>'/#/register','callbackUri'=>'http://localhost:8000/idSiteResponse']);
+    return redirect($url);
+});
+
+$app->get('forgotPassword', function() use ($spapplication) {
+    $url = $spapplication->createIdSiteUrl(['path'=>'/#/forgot','callbackUri'=>'http://localhost:8000/idSiteResponse']);
+    return redirect($url);
+});
+
+
+
+$app->get('logout', function() use ($spapplication) {
+    if(!Session::has('user')) {
+        return redirect('/?test=test');
+    }
+    $url = $spapplication->createIdSiteUrl(['logout'=>true, 'callbackUri'=>'http://localhost:8000/idSiteResponse']);
+
+    return redirect($url);
+});
+
+$app->get('idSiteResponse', function() use ($spapplication, $app) {
+    $response = $spapplication->handleIdSiteCallback($_SERVER['REQUEST_URI']);
+
+    switch($response->status) {
+        case 'AUTHENTICATED' :
+            Session::put('user', $response->account);
+            Session::flash('notice', 'You have been logged in');
+            return redirect('/');
+            break;
+        case 'LOGOUT' :
+            Session::forget('user');
+            Session::flash('notice', 'You have been logged out');
+            return redirect('/');
+            break;
+
+    }
+
+});
---
+++ resources/views/welcome.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <title>Lumen</title>
+
+    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 96px;
+            margin-bottom: 40px;
+        }
+
+        .quote {
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <nav>
+        <?php if( Session::has('user') ): ?>
+            Welcome, <?php echo Session::get('user')->givenName; ?> |
+            <a href="/logout">Logout</a>
+        <?php else : ?>
+            <a href="/login">Login</a> |
+            <a href="/register">Register</a> |
+            <a href="/forgotPassword">Forgot Password</a>
+        <?php endif; ?>
+
+
+    </nav>
+    <div class="content">
+        <div class="title">Lumen.</div>
+    </div>
+</div>
+</body>
+</html>
